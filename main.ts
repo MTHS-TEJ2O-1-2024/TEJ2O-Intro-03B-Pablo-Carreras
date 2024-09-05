adafc6f5dc06dac499b95e8f32d0b6129801c914
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-05 11:25:04 -0400

    change comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo Carreras
+ * Created on: Sep,5 2024
+ * This program shwos Hello, World!
 */
 
 basic.showString('Hello, World!')
